70d058cf034c4db03849030e1eb7028ab63c768f
davidluiz91 <dvdluiz1991@gmail.com>
2023-01-02 17:26:48 -0300

    start exerc01

---
+++ src/exercicios/exerc01/Rectangle.java
@@ -0,0 +1,23 @@
+package exercicios.exerc01;
+/**
+ * Fazer um programa para ler os valores da largura e altura
+ * de um retângulo. Em seguida, mostrar na tela o valor de
+ * sua área, perímetro e diagonal. Usar uma classe como
+ * mostra no projeto ao lado.
+ *
+ * Rectangle
+ * - width: double
+ * - Height: double
+ * + Area(): double
+ * + Perimeter(): double
+ * + Diagonal(): double
+ * Exemplo:
+ * Enter rectangle width and height:
+ * 3.00
+ * 4.00
+ * AREA = 12.00
+ * PERIMETER = 14.00
+ * DIAGONAL = 5.00
+ * **/
+public class Rectangle {
+}
